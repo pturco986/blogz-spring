43e08c756d764089c421c2fba4bf75c61bfeef65
Patrick <pturco986@gmail.com>
2016-11-10 19:55:32 -0600

    Altered index to add basic loop, implemented signup

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -1,9 +1,10 @@
 package org.launchcode.blogz.controllers;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthenticationController extends AbstractController {
+	
+	@Autowired
+	private UserDao userDao;
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupForm() {
@@ -21,8 +25,36 @@ public class AuthenticationController extends AbstractController {
 	public String signup(HttpServletRequest request, Model model) {
 		
 		// TODO - implement signup
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String verify = request.getParameter("verify");
 		
-		return "redirect:blog/newpost";
+		
+		if (User.isValidUsername(username) && User.isValidPassword(password) && password.equals(verify)){
+			//This is if everything passes
+			//need to add user to database
+			User user = new User(username, password);
+			userDao.save(user);
+			return "newpost";
+		} 
+		
+		if (!User.isValidPassword(password) || password == null) {
+			//This occurs if the password isn't valid
+			model.addAttribute("password_error", "Your username is invalid");
+		}
+			
+		if (!User.isValidUsername(username) || username == null) {
+			//this occurs if the username isn't valid
+			model.addAttribute("username_error", "Your password is invalid");
+			
+		} 
+		
+		if (!password.equals(verify) || verify == null) {
+			//This occurs if the passwords do not match
+			model.addAttribute("verify_error", "Your passwords do not match");
+
+		} 
+		return "signup";
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
