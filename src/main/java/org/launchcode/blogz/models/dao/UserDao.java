241b73c222be4a1d0ea5bb0835008b3d297f9301
Patrick <pturco986@gmail.com>
2016-11-25 16:13:35 -0600

    Removed some spacing.

--- src/main/java/org/launchcode/blogz/models/dao/UserDao.java
+++ src/main/java/org/launchcode/blogz/models/dao/UserDao.java
@@ -14,8 +14,6 @@ public interface UserDao extends CrudRepository<User, Integer> {
 
     User findByUid(int uid);
     
-    
-    
     List<User> findAll();
     
     // TODO - add method signatures as needed
